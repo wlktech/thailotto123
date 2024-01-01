ad8fc318021a22073d29b8749c4c73061073d01f
Wai Linn Kyaw <108474411+wlktech@users.noreply.github.com>
2024-01-02 01:41:33 +0630

    a

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -28,7 +28,7 @@ export default function Login() {
                     type="email"
                     autoComplete="email"
                     required
-                    className="px-3 block w-full rounded-md border-2 py-1.5 text-gray-900 border-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="px-3 block w-full rounded-md border-2 py-1.5 text-gray-900 border-gray-400 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
@@ -46,7 +46,7 @@ export default function Login() {
                     type="password"
                     autoComplete="current-password"
                     required
-                    className="px-3 block w-full rounded-md border-2 border-gray-900 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="px-3 block w-full rounded-md border-2 border-gray-400 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
                 <div className="text-sm text-end mt-2">
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -43,7 +43,7 @@ export default function Register() {
                         type="email"
                         autoComplete="email"
                         required
-                        className="px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        className="px-3 block w-full rounded-md border-2 border-gray-400 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                     </div>
                 </div>
@@ -61,7 +61,7 @@ export default function Register() {
                         type="password"
                         autoComplete="current-password"
                         required
-                        className="px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        className="px-3 block w-full rounded-md border-2 border-gray-400 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                     </div>
                 
